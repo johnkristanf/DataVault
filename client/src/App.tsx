11b965b287ad3476c92817a5fcf6fae179027139
John Kristan <jkpogz09@gmail.com>
2024-01-08 22:51:11 +0800

    refactor: added the my vault route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import React, { Suspense } from "react"
+import React, { Suspense } from "react";
 
 const LandingPage = React.lazy(() => import("./pages/LandingPage"))
-
+const VaultPage = React.lazy(() => import("./pages/VaultPage"))
 
 function App() {
 
-  return(
-
+  return (
     <BrowserRouter basename="/">
-
       <Suspense fallback={<div> Loading ..... </div>}>
-
         <Routes>
+
           <Route path="/" Component={LandingPage} />
+          <Route path="/my-vault" Component={VaultPage}/>
+
         </Routes>
-
       </Suspense>
-
     </BrowserRouter>
-
-  )
+  );
 }
+
 
 export default App
