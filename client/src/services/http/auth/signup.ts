21e9b8f115a09880127506ab82f7448433676a39
John Kristan <jkpogz09@gmail.com>
2024-01-08 23:04:10 +0800

    build: implement the http req signup

--- client/src/services/http/auth/signup.ts
+++ client/src/services/http/auth/signup.ts
@@ -6,7 +6,6 @@ export const signup = async (signupCredentials: SignupCredTypes) => {
     try {
         
         const response = await axios.post('http://localhost:900/user/signup', signupCredentials)
-        console.log('signup res', response.data)
         if(response.data) return response.data
         
     } catch (error) {
